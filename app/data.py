7c65f07b496b7aeb016eb13f68387b264d422357
David Close <114041624+DavidClose0@users.noreply.github.com>
2025-03-25 12:44:19 -0700

    Use API key

--- app/data.py
+++ app/data.py
@@ -1,10 +1,11 @@
 """
 This module contains a function to get data from the Neo API.
 """
+import os
 import streamlit as st
 import requests
 
-@st.cache_data(ttl=60)  # Cache data for 1 minute
+@st.cache_data(ttl=3600)  # Cache data for 1 hour
 def get_data(date: str) -> dict:
     """
     Gets data from Neo API for a given date.
@@ -13,7 +14,8 @@ def get_data(date: str) -> dict:
     Returns:
         dict: Dictionary containing data from Neo API.
     """
-    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={date}&end_date={date}&api_key=DEMO_KEY"
+    api_key = os.environ["API_KEY"]
+    url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={date}&end_date={date}&api_key={api_key}"
 
     try:
         data = requests.get(url, timeout=10).json()
