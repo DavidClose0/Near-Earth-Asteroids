fa58dec09c436cc3c7417bc2894fed8965a4c332
David Close <114041624+DavidClose0@users.noreply.github.com>
2025-03-25 10:56:33 -0700

    Fix test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,11 +7,12 @@ from streamlit.testing.v1 import AppTest
 class Test(TestCase):    
     def test_ui_title_and_header(self):
         """
-        Test UI title and header.
+        Test UI sidebar title and header.
         """
         at = AppTest.from_file("./app/main.py")
         at.run()
 
-        assert at.title[0].value.startswith("Near Earth Objects")
+        # Check if sidebar title and header are correct
+        assert at.title[0].value == "Navigation"
         assert at.header[0].value.startswith("Near Earth Objects for")
         assert not at.exception
