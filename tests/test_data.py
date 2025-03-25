62cc3f2f5595484ad170c7a7312607ed163171a6
David Close <114041624+DavidClose0@users.noreply.github.com>
2025-03-24 22:24:44 -0700

    Add test for data.py

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from data import get_data
+
+class Test(TestCase):
+    def test_get_data(self):
+        """
+        Test get_data function.
+        """
+        date = "2025-01-01"
+        data = get_data(date)
+
+        # Check if data contains near_earth_objects for the given date
+        neos = data["near_earth_objects"][date]
+        assert len(neos) > 0
+        assert "id" in neos[0]
+
+        
